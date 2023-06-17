873407b20c1ddf92d139dc1a8045a1ab5d0f817b
JamieMac <Jamiejmackenzie@gmail.com>
2023-06-17 14:25:02 +0100

    Add Eslint and fix eslint problems

--- src/app.ts
+++ src/app.ts
@@ -1,76 +1,87 @@
-import express from "express";
-//const xss = require("xss");
-import helmet from "helmet";
-import rateLimit from "express-rate-limit";
-const mongoSanitize = require("express-mongo-sanitize");
-const hpp = require("hpp");
-const morgan = require("morgan");
-const cors = require("cors");
-import { Request, Response, NextFunction } from "express";
-//const globalErrorHandler = require('./controllers/errorContorller');
-//const AppError = require('./utils/appError');
+import express, { Request, Response } from 'express';
+// const xss = require("xss");
+import helmet from 'helmet';
+import rateLimit from 'express-rate-limit';
 
-const goCardlessRouter = require("./routers/goCardlessRouter");
-///////////////////////////////////////////////////////////////
+const mongoSanitize = require('express-mongo-sanitize');
+const hpp = require('hpp');
+const morgan = require('morgan');
+const cors = require('cors');
+// const globalErrorHandler = require('./controllers/errorController');
+// const AppError = require('./utils/appError');
+// Set rate limiter //
+const limiter = rateLimit({
+  windowMs: 60 * 60 * 1000, // 60 minutes
+  max: 200, // limit each IP to 200 requests per
+  // hour
+  // windowMs
+  message: 'Too many requests from this IP, please try again after an hour',
+});
+
+const goCardlessRouter = require('./routers/goCardlessRouter');
+/// ////////////////////////////////////////////////////////////
 const app = express();
+//  apply to all requests to routs that include /api
+app.use('/api', limiter);
 
 // Data sanitization against NoSQL query injection
 app.use(mongoSanitize());
 // Data sanitization against XSS attacks
-//app.use(xss());
+// app.use(xss());
 // protect against HTTP Parameter Pollution attacks
-// without white list will only sort with the last parameter in the query string
+// without whitelist will only sort with the last parameter in the query
+// string
 app.use(
   hpp({
     whitelist: [],
-  })
+  }),
 );
 // serve static files
 app.use(express.static(`${__dirname}/public`));
-//SET SECURITY HTTP HEADERS //
+// SET SECURITY HTTP HEADERS //
 app.use(helmet());
-//Set Cross origin policy
+// Set Cross origin policy
 app.use(cors());
 // Development Logging
-if (process.env.NODE_ENV !== "production") {
+if (process.env.NODE_ENV !== 'production') {
   // morgan is a logger function
-  app.use(morgan("dev"));
+  app.use(morgan('dev'));
 }
 
-//app.use(express.text({type: "application/json"}));
-app.use("/api/gocardless", goCardlessRouter);
+// app.use(express.text({type: "application/json"}));
+app.use('/api/gocardless', goCardlessRouter);
 
 // Return 200 for "/" route to fix AWS warning
-app.use("/", (req, res, next) => {
-  res.status(200).json({ message: "App running successfully" });
+app.use('/', (req, res) => {
+  res.status(200).json({ message: 'App running successfully' });
 });
-////////////////// Mount Routers ////////////////////////
-// handle incoming webhook from GoCardless
 
-////////////////// Handle all undefined routes * /////////////////////
+/// /////////////// Handle all undefined routes * /////////////////////
 // Handling undefined routes with "*" and .all for all methods
-// this must be after all the possible rout handlers as they are matched in order
+// this must be after all the possible rout handlers as they are matched
+// in order
 
-app.all("*", (req: Request, res: Response, next: NextFunction) => {
-  // Create a new err instance and pass it to next() //
+app.all('*', (req: Request, res: Response) => {
+  // Create a new error instance and pass it to next() //
   /*  const err = new Error();
-  err.status = 'fail';
-  err.message = `Can't find ${req.url} on this server`;
-  err.statusCode = 404;
-  // if next ever has an argument it is always an error
-  //Express will then exit the normal flow and jump to the error handler
-  */
+   err.status = 'fail';
+   err.message = `Can't find ${req.url} on this server`;
+   err.statusCode = 404;
+   // if next ever has an argument it is always an error
+   //Express will then exit the normal flow
+   and jump to the error handler
+   */
   //  next(new AppError(`Can't find ${req.url} on this server!`, 400));
-  res.status(404).json({ message: "Not Found" });
+  res.status(404).json({ message: 'Not Found' });
 });
 
-//////////////////////ERROR MIDDLEWARE///////////////////////
-////////////////////////////////////////////////////////////
+/// ///////////////////ERROR MIDDLEWARE///////////////////////
+/// /////////////////////////////////////////////////////////
 
-//This will be the Express built in error handler
-//app.use(globalErrorHandler);
+// This will be the Express built in error handler
+// app.use(globalErrorHandler);
 
-///////////////////////////////////////////////////////////
-///////////////////////////////////////////////////////////
+/// ////////////////////////////////////////////////////////
+/// ////////////////////////////////////////////////////////
 
 export default app;
